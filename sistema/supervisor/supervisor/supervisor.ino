22af4cf27061a665838448848b3dc60ece1ee674
Nirvan Yang <yangnyd@gmail.com>
2025-05-26 12:11:24 -0300

    final

--- sistema/supervisor/supervisor/supervisor.ino
+++ sistema/supervisor/supervisor/supervisor.ino
@@ -2,10 +2,33 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
+#define BUTTON_PIN 12          // Pino D12 (corresponde ao bit 4 do PORTB)
+#define BUTTON_MASK (1 << 4)   // Máscara para o bit 4 do PORTB
+#define INTERRUPT_PIN PCINT4   // PCINT4 corresponde ao pino D12
+
+volatile unsigned long millis_timer1 = 0;
+volatile unsigned long lastInterruptTime = 0;
+volatile bool buttonPressed = false;
+int debounceTime = 50; // tempo de debounce em ms
+float tempo_ant = 0;
+
+int enviou = 0; 
+int parada = 1;
+int status = 0; 
+int mudou = 0;
+
 // variaveis glovais para controle do motor
 int pwm_d5 = 0;
 int pwm_d6 = 0;
-int enviou = 0; 
+
+float temperatura = 0;
+float inclinacao = 0;
+int presenca = 0;
+int nivel = 0;
+int statusF;
+int v1 = 0;
+int v2 = 0;
+int blocos = 0;
 
 // Implementação da USART
 void USART_init(unsigned int ubrr) {
@@ -107,9 +130,6 @@ void enviar_fabrica_Velocidade(){
     response += c;
   }
   const char* resp = response.c_str();
-  USART_send_string("Resposta do escravo: ");
-  USART_send_string(resp);
-  USART_send_string("\n");
 }
 
 void pedir_dados(){
@@ -131,29 +151,233 @@ void pedir_dados(){
     response += c;
   }
   const char* resp = response.c_str();
-  USART_send_string("Resposta do escravo: ");
+  char* token;
+  int campo = 0;
+  token = strtok(resp, ";");
+    
+  while (token != NULL) {
+        switch (campo) {
+            case 0:
+                temperatura = atoi(token);
+                if(temperatura < 10 || temperatura > 40){
+                    mudou = 1;
+                }
+                break;
+            case 1:
+                inclinacao = atoi(token);
+                if(inclinacao){
+                    mudou = 1;
+                }
+                break;
+            case 2:
+                presenca = atoi(token);
+                break;
+            case 3:
+                nivel = atoi(token);
+                break;
+            case 4:
+                statusF = atoi(token);
+                break;
+            case 5:
+                v1 = atof(token);
+                break;
+            case 6:
+                v2 = atof(token);
+                break;
+            case 7:
+                blocos = atoi(token);
+                break;
+        }
+
+        token = strtok(NULL, ";");
+        campo++;
+    }
+}
+
+bool continua_producao(){
+  // Envia a string para o escravo
+  char msg;
+  msg = 'C'; 
+
+    
+  Wire.beginTransmission(0x40);
+  Wire.write(msg);
+  Wire.endTransmission();
+
+  // Solicita resposta (lê até 32 bytes ou encontrar '\n')
+  Wire.requestFrom(0x40, 32);
+  String response = "";
+  while (Wire.available()) {
+    char c = Wire.read();
+    if (c == '\n') break;  // Para ao encontrar o terminador
+    response += c;
+  }
+  const char* resp = response.c_str();
+  USART_send_string("Resposta do escravo continuar: ");
   USART_send_string(resp);
   USART_send_string("\n");
 
+  if( resp[0] == 'C'){
+    return true;
+  }else{
+    return false;
+  }
+}
 
+bool pedir_parada(){
+  // Envia a string para o escravo
+  char msg;
+  msg = 'P'; 
+
+  Wire.beginTransmission(0x40);
+  Wire.write(msg);
+  Wire.endTransmission();
+  USART_send_string("Parada i2C: \n");
+
+  // Solicita resposta (lê até 32 bytes ou encontrar '\n')
+  Wire.requestFrom(0x40, 32);
+  String response = "";
+  while (Wire.available()) {
+    char c = Wire.read();
+    if (c == '\n') break;  // Para ao encontrar o terminador
+    response += c;
+  }
+  const char* resp = response.c_str();
+  USART_send_string("Resposta do escravo parada: ");
+  USART_send_string(resp);
+  USART_send_string("\n");
+  if( resp[0] == 'P'){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+void setup_botao(){
+  // Configura o pino D12 como entrada com pull-up
+  DDRB &= ~BUTTON_MASK;    // Configura como entrada (clear no bit)
+  //PORTB |= BUTTON_MASK;    // Ativa pull-up (set no bit)
+  
+  // Configura a interrupção por mudança de pino (PCINT)
+  PCICR |= (1 << PCIE0);    // Habilita o banco de interrupção PCINT0 (pinos D8-D13)
+  PCMSK0 |= (1 << PCINT4);  // Habilita interrupção específica para o pino D12 (PCINT4)
+}
+
+// Rotina de serviço de interrupção para PCINT0 (pinos D8-D13)
+ISR(PCINT0_vect) {
+  unsigned long currentTime = millis_timer1;
+
+  // Verifica debounce
+  if (currentTime - lastInterruptTime > debounceTime) {
+    // Lê o estado atual do botão diretamente do registrador
+    bool currentState = (PINB & BUTTON_MASK) ? false : true; // Inverte a lógica pull-up
+    
+    if (!currentState) {
+      if(buttonPressed){
+        buttonPressed = false;
+        parada = 0;
+        USART_send_string("Parada desfeita");
+      }else {
+        buttonPressed = true;
+        parada = 1;
+        Serial.println("Parada solicitada");
+      }
+      
+      
+    } else {
+      //Serial.println("Botão liberado (ISR)");
+      //Serial.println(buttonPressed);
+    }
+  }
+  lastInterruptTime = currentTime;
+}
+
+ISR(TIMER1_COMPA_vect) {
+  millis_timer1++;
+}
+
+void controle_parada(){
+    if(!buttonPressed && (parada == 0)){
+        parada = 1; 
+        if(continua_producao()){
+            USART_send_string("Parada desfeita com sucesso!\n");
+            status = 1;
+        }else{
+            USART_send_string("Nao foi possivel continuar a producao\n");
+        }
+    }else if (buttonPressed && (parada == 1)){
+        parada = 0;
+        if(pedir_parada()){
+            USART_send_string("Parada realizada com sucesso!\n");
+            status = 0;
+        }else{
+            USART_send_string("Nao foi possivel parar a producao\n");
+        }
+        
+    }
+}
+
+void contador_mili(){
+  // Configura Timer1 para CTC (Clear Timer on Compare Match)
+  TCCR1A = 0;              // Normal operation
+  TCCR1B = 0;
+
+  // Modo CTC: WGM12 = 1
+  TCCR1B |= (1 << WGM12);
+
+  // Prescaler = 64
+  TCCR1B |= (1 << CS11) | (1 << CS10);
+
+  // Valor de comparação para 1 ms:
+  // 16 MHz / 64 = 250.000 ticks por segundo
+  // 250.000 ticks / 1000 = 250 ticks por milissegundo
+  OCR1A = 250 - 1;
+
+  // Habilita interrupção por comparação
+  TIMSK1 |= (1 << OCIE1A);
+}
+
+
+void temp_ant(){
+    char msg[500];
+    if(millis_timer1 - tempo_ant > 3000){
+        if(temperatura < 10 || temperatura > 40 && mudou){
+            USART_send_string("TEMPERATURA CRÍTICA!\n");
+            mudou = 0;
+        }
+        else if(inclinacao && mudou){
+            USART_send_string("MADEIRA FORA DO EIXO!\n");
+            mudou = 0;
+        }
+        USART_send_string("Status da Fabrica: \n");
+        USART_send_string("Temperatura: ");
+        send_number(temperatura);
+        USART_send_string(" Inclinacao: ");
+        send_number(inclinacao);
+        USART_send_string(" Presenca: ");
+        send_number(presenca);
+        sprintf(msg, "\nNível: %d Status: %d V1: %d V2: %d Blocos: %d\n", nivel, statusF, v1, v2, blocos);
+        USART_send_string(msg);
+        tempo_ant = millis_timer1;
+    } 
 }
 
 void setup() {
   Wire.begin();  // Inicia como mestre
+  tempo_ant = millis_timer1;
   //Serial.begin(9600);
+  contador_mili();
+  setup_botao();
   USART_init(103);
   USART_send_string("Mestre I2C iniciado.\n");
 }
 
 void loop() {
-  enviar_fabrica_Velocidade();
-  pedir_dados();
-  controle_velocidade_motorD5();
-  controle_velocidade_motorD6();
-  USART_send_string("PWM D5: ");
-  send_number(pwm_d5);
-  USART_send_string("PWM D6: ");
-  send_number(pwm_d6);  
-  USART_send_string("\n");
-  delay(700);
+    enviar_fabrica_Velocidade();
+    pedir_dados();
+    controle_velocidade_motorD5();
+    controle_velocidade_motorD6();
+    controle_parada();
+    temp_ant();
+    delay(500);
 }
